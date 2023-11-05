22f296f4ecc0152658f725dacedf808e2548434e
pfistdo <pfistdo1@students.zhaw.ch>
2023-11-05 14:05:59 +0100

    Added post request

--- app.py
+++ app.py
@@ -1,54 +1,80 @@
 from flask import Flask
-from flask_restful import Api, Resource
+from flask_restful import Api, Resource, reqparse
 import mysql.connector
 
 app = Flask(__name__)
 api = Api(app)
 # DB connection
-cnx = mysql.connector.connect(user='root', password='admin',
-                                    host='127.0.0.1',
-                                    database='poop_tracker')
+user = 'root'
+password = 'admin'
+host = '127.0.0.1'
+database = 'poop_tracker'
 
 ## ####################################################
 ## Resources
 ## ####################################################
 class Poop(Resource):
+    # Get a single poop
     def get(self, poop_id):
         try:
-            cursor = cnx.cursor(dictionary=True)
-            query = ("SELECT * FROM poop WHERE ID_poop = %s")
-            cursor.execute(query, (poop_id,))
-            result = cursor.fetchone()
+            with mysql.connector.connect(user=user, password=password,
+                                        host=host,
+                                        database=database) as cnx:
+                with cnx.cursor(dictionary=True) as cursor:
+                    query = ("SELECT * FROM poop WHERE ID_poop = %s")
+                    cursor.execute(query, (poop_id,))
+                    result = cursor.fetchone()
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
+            return "An error occurred while processing the request", 500
         finally:
             cursor.close()
             cnx.close()
-        return result
+        return result, 200
     
+    # Insert a new poop
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('weight')
+            data = parser.parse_args()
+            with mysql.connector.connect(user=user, password=password,
+                                        host=host,
+                                        database=database) as cnx:
+                with cnx.cursor() as cursor:
+                    query = "INSERT INTO poop (weight) VALUE (%s);"
+                    cursor.execute(query, (data['weight'],))
+                    cnx.commit()
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+            return "An error occurred while processing the request", 500
+        return "Poop data inserted successfully", 201
+
 class PoopList(Resource):
+    # Get all poops
     def get(self):
         try:
-            cursor = cnx.cursor(dictionary=True)
-            query = ("SELECT * FROM poop;")
-            cursor.execute(query)
-            result = cursor.fetchall()
-            print(result)
+            with mysql.connector.connect(user=user, password=password,
+                                        host=host,
+                                        database=database) as cnx:
+                with cnx.cursor(dictionary=True) as cursor:
+                    query = ("SELECT * FROM poop;")
+                    cursor.execute(query)
+                    result = cursor.fetchall()
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
+            return "An error occurred while processing the request", 500
         finally:
             cursor.close()
             cnx.close()
-        return result
+        return result, 200
 
 ## ####################################################
 ## Endpoints
 ## ####################################################
-api.add_resource(Poop, '/poop/<poop_id>')
-api.add_resource(PoopList, '/poop', '/poop/')
+api.add_resource(Poop, '/poop/<poop_id>', '/poop', '/poop/')
+api.add_resource(PoopList, '/poops', '/poops/')
 
 
 if __name__ == '__main__':
-
-
     app.run(debug=True)
