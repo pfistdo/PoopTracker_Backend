949fae21810b1ef268a27af030ad621063a3b8af
pfistdo <pfistdo1@students.zhaw.ch>
2023-11-05 12:59:57 +0100

    Basic implementation with local DB

--- app.py
+++ app.py
@@ -1,26 +1,54 @@
 from flask import Flask
 from flask_restful import Api, Resource
+import mysql.connector
 
 app = Flask(__name__)
 api = Api(app)
-
-POOP = {
-    '1': {'500g': '15c'},
-    '2': {'350g': '18c'},
-}
+# DB connection
+cnx = mysql.connector.connect(user='root', password='admin',
+                                    host='127.0.0.1',
+                                    database='poop_tracker')
 
 ## ####################################################
-## Helper functions
+## Resources
 ## ####################################################
 class Poop(Resource):
     def get(self, poop_id):
-        return POOP[poop_id]
+        try:
+            cursor = cnx.cursor(dictionary=True)
+            query = ("SELECT * FROM poop WHERE ID_poop = %s")
+            cursor.execute(query, (poop_id,))
+            result = cursor.fetchone()
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+        finally:
+            cursor.close()
+            cnx.close()
+        return result
+    
+class PoopList(Resource):
+    def get(self):
+        try:
+            cursor = cnx.cursor(dictionary=True)
+            query = ("SELECT * FROM poop;")
+            cursor.execute(query)
+            result = cursor.fetchall()
+            print(result)
+        except mysql.connector.Error as err:
+            print("Something went wrong: {}".format(err))
+        finally:
+            cursor.close()
+            cnx.close()
+        return result
 
 ## ####################################################
 ## Endpoints
 ## ####################################################
 api.add_resource(Poop, '/poop/<poop_id>')
+api.add_resource(PoopList, '/poop', '/poop/')
 
 
 if __name__ == '__main__':
+
+
     app.run(debug=True)
