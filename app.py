ad3a070f03ad9453cb0858d22e517d6cbb6ba4a6
pfistdo <pfistdo1@students.zhaw.ch>
2023-11-05 15:45:58 +0100

    Moved db config to config file

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse
 import mysql.connector
+import json
+import socket
+import os
 
 app = Flask(__name__)
 api = Api(app)
-# DB connection
-user = 'root'
-password = 'admin'
-host = '127.0.0.1'
-database = 'poop_tracker'
+
+# Determine the environment (local or pythonanywhere)
+if "PC22" in socket.gethostname():
+    environment = "local"
+else:
+    environment = "pythonanywhere"
+
+# Load the configuration from the JSON file
+if environment == "local":
+    config_file_path = "config.json"
+else:
+    config_file_path = "/home/pfistdo/mysite/config.json"
+with open(config_file_path) as config_file:
+    config = json.load(config_file)
+
+# Get the database configuration
+db_config = config[environment]
+user = db_config["user"]
+password = db_config["password"]
+host = db_config["host"]
+database = db_config["database"]
 
 ## ####################################################
 ## Resources
@@ -27,11 +46,8 @@ class Poop(Resource):
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
             return "An error occurred while processing the request", 500
-        finally:
-            cursor.close()
-            cnx.close()
-        return result, 200
-    
+        return result, 200  # The cursor will be automatically closed here
+
     # Insert a new poop
     def post(self):
         try:
@@ -48,7 +64,8 @@ class Poop(Resource):
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
             return "An error occurred while processing the request", 500
-        return "Poop data inserted successfully", 201
+        return "Poop data inserted successfully", 201  # The cursor will be automatically closed here
+
 
 class PoopList(Resource):
     # Get all poops
@@ -64,9 +81,6 @@ class PoopList(Resource):
         except mysql.connector.Error as err:
             print("Something went wrong: {}".format(err))
             return "An error occurred while processing the request", 500
-        finally:
-            cursor.close()
-            cnx.close()
         return result, 200
 
 ## ####################################################
@@ -76,5 +90,8 @@ api.add_resource(Poop, '/poop/<poop_id>', '/poop', '/poop/')
 api.add_resource(PoopList, '/poops', '/poops/')
 
 
+## ####################################################
+## Main
+## ####################################################
 if __name__ == '__main__':
     app.run(debug=True)
