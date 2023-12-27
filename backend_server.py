716655207c73d5d01f6bd71c8c09c8d63307c338
pfistdo <pfistdo1@students.zhaw.ch>
2023-12-27 17:30:31 +0100

    Listening to Heroku port

--- backend_server.py
+++ backend_server.py
@@ -1,6 +1,7 @@
+import os
 from routes import app
 
 # Start the server
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="localhost", port=8000 or os.environ.get('PORT', 17995))
