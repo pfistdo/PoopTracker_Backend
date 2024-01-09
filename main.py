aef52185bdb92ab2d9a64547be8a39b8e4c8444b
Dominic Pfister <pfistdo1@students.zhaw.ch>
2024-01-09 20:39:48 +0100

    Removed pythonanywhere port

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ import uvicorn
 
 # Start the server
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000 or os.environ.get('PORT', 17995))
+    uvicorn.run(app, host="localhost", port=8000)
